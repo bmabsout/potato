b839af2fda3b3f4a18c563c4eb1e6c38189ab0a1
Bassel Mabsout <iggybibi@hotmail.com>
2024-07-22 22:09:39 +0100

    updated potato to support many systems!

--- flake.nix
+++ flake.nix
@@ -1,27 +1,23 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
     potatoImg.url = "https://www.shutterstock.com/image-photo/young-potato-isolated-on-white-260nw-630239534.jpg";
     potatoImg.flake = false;
   };
 
   nixConfig = {
     extra-substituters = "https://bmabsout.cachix.org";
-
     extra-trusted-public-keys = "bmabsout.cachix.org-1:/GhCEayGQ3NHMIlJiUelQrLtHHXVdGjHtyDz32xNAo4=";
   };
 
-  outputs = { self, nixpkgs, potatoImg }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      defaultApp.x86_64-linux = {
-	type="app";
-        program = "${pkgs.writeShellApplication {
-		name= "potato";
-		text= "${pkgs.feh}/bin/feh ${potatoImg}";
-	}}/bin/potato";
+  outputs = { self, nixpkgs, flake-utils, potatoImg }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      defaultApp = with nixpkgs.legacyPackages.${system}; {
+        type="app";
+        program = "${writeShellApplication {
+          name = "potato";
+          text = "${feh}/bin/feh ${potatoImg}";
+        }}/bin/potato";
       };
-    };
+    });
 }
